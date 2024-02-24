f7743a2d2125329dc1dde6440e41aa4b1fc57bdc
Nikola Barać <nikola.barac12345@gmail.com>
2024-02-24 14:10:35 +0100

    adding data

---
+++ loan_status_data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 24 14:03:24 2024
+
+@author: Nikola
+"""
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('D:/Python_e/loan_status_prediction/loan_data.csv')
+
